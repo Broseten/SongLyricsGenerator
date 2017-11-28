b1f217dc4e7458021c76e410d4fddcabd02951c9
VojtechBruza <vojtech.bruza@gmail.com>
2017-11-28 21:56:52 +0100

    Initial commit with processing

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+/**
+ * @author Vojtech Bruza
+ */
+public class Main extends PApplet {
+    public static void main(String[] args){
+        PApplet.main("Main", args);
+    }
+
+    public void settings(){
+//        fullScreen();
+        size(1024,576);
+    }
+
+    public void setup() {
+        noStroke();
+        background(0);
+    }
+
+    public void draw() {
+
+    }
+
+}
