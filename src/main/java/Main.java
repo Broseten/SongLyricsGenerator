5348c342a859d473fb3e3f5431eb1580f8410c8f
VojtechBruza <vojtech.bruza@gmail.com>
2017-11-28 22:48:07 +0100

    Added Minim lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
 import processing.core.PApplet;
 
 /**
+ * TODO package
+ * TODO draw text - some class hierarchy
+ * TODO analyze sound - try to do it offline
+ * TODO try to generate the video completely offline
+ * TODO skipping when playing live
+ *
  * @author Vojtech Bruza
  */
 public class Main extends PApplet {
@@ -13,13 +21,18 @@ public class Main extends PApplet {
         size(1024,576);
     }
 
+    private Minim minim;
+    private AudioPlayer song;
+
     public void setup() {
         noStroke();
         background(0);
+        minim = new Minim(new MinimFileSystemHandler());
+        song = minim.loadFile(args[0]);
+        song.play();
     }
 
     public void draw() {
-
     }
 
 }
